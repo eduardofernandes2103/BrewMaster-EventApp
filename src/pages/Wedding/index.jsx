8cd27f0d73f1117017c8b8bccc975f99c412e4ed
eduardofernandes2103 <eduardofernandes21031@gmail.com>
2021-06-15 00:11:02 -0300

    feat: create a Wedding page

--- src/pages/Wedding/index.jsx
+++ src/pages/Wedding/index.jsx
@@ -1,5 +1,41 @@
+import { Link } from 'react-router-dom'
+import Container from './styles'
+import { IoIosBeer } from 'react-icons/io'
+
+
 const Wedding = () => {
-    return <div>Wedding</div>
+    return (
+
+        
+
+        <Container>
+
+            <div className="topBar">
+                
+                <div className="title">
+                    <h1>BrewerMaster </h1>
+                    <div className="icon">
+                        <IoIosBeer/>
+                    </div>
+                </div>
+
+                <div className="links">
+                    <Link to='/catalog'> Go to Catalog</Link>
+                </div>
+
+            </div>
+
+            <div className="homeText">
+                <h2>Wedding</h2>
+                
+                <div className="cardsPlace">
+                    Aqui vão ficar os cards
+                </div>
+
+            </div>
+            
+        </Container>
+    )
 }
 
-export default Wedding
+export default Wedding;
